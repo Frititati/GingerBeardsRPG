390fd8c585c02067eec20cdcb40a1bca8b03041f
michelepettinato <michelepettinato@ymail.com>
2016-05-10 01:15:29 +0200

    fixed minor glitch: player disappearing when colliding

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,15 +4,11 @@
 #include <iostream>
 using namespace std;
 
-char playerLook [5];
+char playerLook[] = {'(', '"', '-', '"', ')'};
 char toBePrinted[500000];
 
+
 void Player::playerMovement(int keypressed, Map*& mapEditor){
-	playerLook[0] = '(';
-	playerLook[1] = '"';
-	playerLook[2] = '-';
-	playerLook[3] = '"';
-	playerLook[4] = ')';
 	int tempx = xpossition;
 	int tempy = ypossition;
 	switch(keypressed){
@@ -31,14 +27,11 @@ void Player::playerMovement(int keypressed, Map*& mapEditor){
 	case 5:
 	break;
 	}
-	if(mapEditor->testBorder(xpossition, ypossition, 5)){
-		mapEditor->drawCharacter(xpossition, ypossition,playerLook);
-		//mapEditor->mapViewPoint(xpossition,ypossition,toBePrinted);
-		//SetWindowText(textArea, TEXT(toBePrinted));
-	}else{
+	if(!mapEditor->testBorder(xpossition, ypossition, 5)){
 		xpossition = tempx;
 		ypossition = tempy;
 	}
+	mapEditor->drawCharacter(xpossition, ypossition,playerLook);
 }
 void Player::playerPossition(int* x, int* y){
 	*x = xpossition;
